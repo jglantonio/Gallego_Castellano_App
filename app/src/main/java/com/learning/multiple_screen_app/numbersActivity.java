affe2a50d3dd9e88be6881b235875038e301ba3f
jglantonio <joseantoniogonzalezlopez@gmail.com>
2017-03-07 14:44:56 +0100

    Añadiendo numeros a textviews

--- app/src/main/java/com/learning/multiple_screen_app/numbersActivity.java
+++ app/src/main/java/com/learning/multiple_screen_app/numbersActivity.java
@@ -1,10 +1,14 @@
 package com.learning.multiple_screen_app;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.learning.multiple_screen_app.R.id.numbers;
 
 public class numbersActivity extends AppCompatActivity {
 
@@ -12,6 +16,24 @@ public class numbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numbers = new ArrayList<String>();
+        numbers.add("Uno");
+        numbers.add("Dos");
+        numbers.add("Tres");
+        numbers.add("Cuatro");
+        numbers.add("Cinco");
+        numbers.add("Seis");
+        numbers.add("Siete");
+        numbers.add("Ocho");
+        numbers.add("Nueve");
+        numbers.add("Diez");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        TextView tV = new TextView(this);
+        tV.setText(numbers.get(0));
+        rootView.addView(tV);
+
     }
 
 }
